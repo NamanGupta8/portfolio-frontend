e470d9b0072deb9a822f0481cd7869d94801356d
Naman Gupta <42265989+NamanGupta8@users.noreply.github.com>
2022-09-09 00:14:17 +0530

    pages setup
    Includes landing page

--- portfolio/src/app/components/navbar/navbar.component.ts
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  NavigationStart,
+  Router,
+  Event as NavigationEvent,
+} from '@angular/router';
+import { filter, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   opened: boolean = false;
+  background: string = '!bg-second-image';
 
-  constructor() {}
+  constructor(
+    private _router: Router,
+    private _changeDetectorRef: ChangeDetectorRef
+  ) {
+    this._router.events
+      .pipe(finalize(() => this._changeDetectorRef.markForCheck()))
+      .subscribe((event: NavigationEvent) => {
+        if (event instanceof NavigationStart) {
+          switch (event.url) {
+            case '/dashboard/home':
+              this.background = '!bg-second-image';
+              break;
+            case '/dashboard/about':
+              this.background = '!bg-third-image';
+              break;
+            case '/dashboard/skills':
+              this.background = '!bg-first-image';
+              break;
+            case '/dashboard/profile':
+              this.background = '!bg-second-image';
+              break;
+            case '/dashboard/contact':
+              this.background = '!bg-first-image';
+              break;
+            default:
+              this.background = '!bg-second-image';
+              break;
+          }
+        }
+      });
+  }
 
   ngOnInit(): void {}
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -5,11 +5,40 @@ module.exports = {
   darkMode: "media", // or 'class'
   theme: {
     screens: {
+      xs: "475px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
       "2xl": "1536px",
+    },
+    extend: {
+      backgroundImage: {
+        "first-image": "url('./assets/images/background.jpg')",
+        "second-image": "url('./assets/images/background2.jpg')",
+        "third-image": "url('./assets/images/background3.jpg')",
+        "avatar-image": "url('./assets/images/avatar.jpg')",
+      },
+      animation: {
+        cursor: "cursor .6s linear infinite alternate",
+        type: "type 1.8s ease-out .8s 1 normal both",
+        "type-reverse": "type 1.8s ease-out 1s infinite alternate-reverse both",
+      },
+      keyframes: {
+        type: {
+          "0%": { width: "0ch" },
+          "5%, 10%": { width: "1ch" },
+          "15%, 20%": { width: "2ch" },
+          "25%, 30%": { width: "3ch" },
+          "35%, 40%": { width: "4ch" },
+          "45%, 50%": { width: "5ch" },
+          "55%, 60%": { width: "6ch" },
+          "65%, 70%": { width: "7ch" },
+          "75%, 80%": { width: "8ch" },
+          "85%, 90%": { width: "9ch" },
+          "95%": { width: "10ch" },
+        },
+      },
     },
     colors: ({ colors }) => ({
       inherit: colors.inherit,
